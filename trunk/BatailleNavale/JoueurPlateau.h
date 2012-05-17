c746f475afecf670a85acd14eb725457cb65344f
olivier.locci@gmail.com <olivier.locci@gmail.com@bd2bedef-95f1-6bb7-0814-7368bef75e63>
2012-05-17 12:24:14 +0000

    Add class joueurplateau

---
+++ trunk/BatailleNavale/JoueurPlateau.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   JoueurPlateau.h
+ * Author: Olivier
+ *
+ * Created on 17 mai 2012, 14:23
+ */
+
+#ifndef JOUEURPLATEAU_H
+#define	JOUEURPLATEAU_H
+
+class JoueurPlateau {
+public:
+    JoueurPlateau();
+    JoueurPlateau(const JoueurPlateau& orig);
+    virtual ~JoueurPlateau();
+private:
+
+};
+
+#endif	/* JOUEURPLATEAU_H */
+
