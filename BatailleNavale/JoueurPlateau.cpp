ffb841f98352f5769dcf03deab2e8698533a0045
olivier.locci@gmail.com <olivier.locci@gmail.com@bd2bedef-95f1-6bb7-0814-7368bef75e63>
2012-05-17 12:24:14 +0000

    Add class joueurplateau

---
+++ BatailleNavale/JoueurPlateau.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   JoueurPlateau.cpp
+ * Author: Olivier
+ * 
+ * Created on 17 mai 2012, 14:23
+ */
+
+#include "JoueurPlateau.h"
+
+JoueurPlateau::JoueurPlateau() {
+}
+
+JoueurPlateau::JoueurPlateau(const JoueurPlateau& orig) {
+}
+
+JoueurPlateau::~JoueurPlateau() {
+}
+
