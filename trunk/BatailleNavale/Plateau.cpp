49a06f6684ecf3c259f513becdc92c1025102c50
olivier.locci@gmail.com <olivier.locci@gmail.com@bd2bedef-95f1-6bb7-0814-7368bef75e63>
2012-05-18 09:06:56 +0000

    Implementation de la classe Plateau

--- trunk/BatailleNavale/Plateau.cpp
+++ trunk/BatailleNavale/Plateau.cpp
@@ -36,14 +36,9 @@ bool Plateau::SetBateau(int taille, int x, int y, ALLIGNEMENT_BATEAU a){
     if(a == HORIZONTAL){
         if(y + taille < this->tailleY){
             Bateau* btmp = new Bateau(x,y,a, taille);
-//TODO:ADD check if we add a boat on another boat
-//            for(int ytmp = y; ytmp < y + taille; ytmp++){
-//                
-//                if(bateauCase = (dynamic_cast<BateauCasePleine*>(theBC))){
-//                    return bateauCase->GetBateau();
-//                    }
-//              
-//            }
+            if(! CheckSetBateau(btmp)){
+                return false;
+            }
             for(int ytmp = y; ytmp < y + taille; ytmp++){
                 delete this->plateau[x][ytmp];
                 this->plateau[x][ytmp] = btmp->AddBateauCase(x,ytmp); 
@@ -53,7 +48,9 @@ bool Plateau::SetBateau(int taille, int x, int y, ALLIGNEMENT_BATEAU a){
     }else if(a == VERTICAL){
         if(x + taille < this->tailleX){
             Bateau* btmp = new Bateau(x,y,a, taille);
-//TODO:ADD check if we add a boat on another boat
+            if(! CheckSetBateau(btmp)){
+                return false;
+            }
             for(int xtmp = x; xtmp < x + taille; xtmp++){
                 delete this->plateau[xtmp][y];
                 this->plateau[xtmp][y] = btmp->AddBateauCase(xtmp,y); 
@@ -102,7 +99,10 @@ bool Plateau::IsAllBateauCoule(){
 int Plateau::GetTailleY(){
     return this->tailleY;
 }
-
+/**
+ * permet de recuperer un tab de string montrant les cases jouées par l'adversaire.
+ * @return 
+ */
 string** Plateau::GetPlateauAdversaire(){
     Plateau* p = this;
     if(p != NULL){
@@ -123,6 +123,10 @@ string** Plateau::GetPlateauAdversaire(){
     return NULL;
 }
 
+/**
+ * Permet de recupérer une représentation du plateau des bateaux du joueur
+ * @return tableau de string
+ */
 string** Plateau::GetPlateauJoueur(){
     Plateau* p = this;
     if(p != NULL){
@@ -143,6 +147,11 @@ string** Plateau::GetPlateauJoueur(){
     return NULL;
 }
 
+/**
+ * Permet de joueur la case X, y. cette fonction doit etre utilisé par l'adversaire.
+ * @param x
+ * @param y
+ */
 void Plateau::JouerBateauCase(int x, int y){
     BateauCasePleine* bateauCase;
     BateauCaseVide* bateauCaseV;
@@ -153,4 +162,13 @@ void Plateau::JouerBateauCase(int x, int y){
     }else if(bateauCaseV = (dynamic_cast<BateauCaseVide*>(theBC))){
        bateauCaseV->SetEtat(EAU);
     }
+}
+
+/**
+ * Permet de verfier si le bateau va etre créer au bonne endroit
+ * @param b
+ * @return true if the boat is well place, false else.
+ */
+bool Plateau::CheckSetBateau(Bateau* b){
+    return true;
 }
