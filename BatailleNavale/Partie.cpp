354a95752dbeefaefaaecfaddbb7bae0b944b6d7
olivier.locci@gmail.com <olivier.locci@gmail.com@bd2bedef-95f1-6bb7-0814-7368bef75e63>
2012-05-18 09:07:30 +0000

    Rajout de la declaration des fonctions de la classe Partie

--- BatailleNavale/Partie.cpp
+++ BatailleNavale/Partie.cpp
@@ -1,13 +1,83 @@
 #include "Partie.h"
 
 using namespace std;
-
+/**
+ * Constructeur
+ */
 Partie::Partie()
 {
-    //ctor
 }
-
+/**
+ * Destructeur
+ */
 Partie::~Partie()
 {
-    //dtor
 }
+/**
+ * Rajoute un joueur à la partie
+ * @param joueur
+ */
+void Partie::AddJoueur(Joueur* joueur){
+    
+}
+/**
+ * Supprime un joueur de la partie
+ * @param joueur
+ */
+void Partie::DelJoueur(Joueur* joueur){
+    
+}
+
+/**
+ * Initialise la partie :
+ * Crés les plateaux, met l'état des joueurs en partie, etc..
+ */
+void Partie::InitPartie(){
+    
+}
+/**
+ * Permet de passer au joueur suivant, change le joueur courant
+ */
+void Partie::JoueurSuivant(){
+    
+}
+/**
+ * Permet au joueur courant de jouer une case
+ * @param x
+ * @param y
+ */
+void Partie::JoueurCourantJoue(int x, int y){
+    
+}
+/**
+ * Permet de renvoyer un tab de string pour le joueur j
+ * @param j
+ * @return 
+ */
+string** Partie::JoueurPlateauPlacementBateau(Joueur* j){
+
+}
+/**
+ * Permet de renvoyer un tab de string pour le joueur j
+ * @param j
+ * @return 
+ */
+string** Partie::JoueurPlateauAdversaire(Joueur* j){
+    
+}
+/**
+ * Permet de renvoyer un tab de string pour le joueur j 
+ * @param j
+ * @return 
+ */
+string** Partie::JoueurPlateauJoue(Joueur* j){
+    
+}
+/**
+ * permet de connaitre le joueur gagnant
+ * @return le joueur gagnant, Null si il y en a pas
+ */
+Joueur* Partie::GetGagnant(){
+    
+}
+
